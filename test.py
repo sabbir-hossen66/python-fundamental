d65a6a4ab29dc7f6ccfb57d4fc33616d71b0f757
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2025-12-17 00:29:48 +0600

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+print("=== Scientific Calculator ===")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+print("5. Modulus (%)")
+print("6. Power (**)")
+
+choice = int(input("Enter your choice (1-6): "))
+# print(choice)
+a= float(input('try to enter number :'))
+b = float(input('enter second number: '))
+
+if(choice ==1):
+    print(a+b)
+else:
+    print("Result:", a ** b)
